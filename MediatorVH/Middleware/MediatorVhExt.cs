a22bf518fa22bc33b73166a76f09455121f53a03
Vasily Himchenko <White_Scorpion@inbox.lv>
2025-05-28 05:56:07 +0300

    Enhance MediatorVhExt with assembly scanning support
    - Added using directives for `System.Reflection` and `Scrutor`.
    - Introduced a new overload of `AddMediatorVh` for `IEnumerable<Assembly>`.
    - Modified existing `AddMediatorVh` to return `services` after scanning.
    - Added `SelectTypesFilter` method to configure handler interfaces.
    - Corrected placement of `WithScopedLifetime()` in `SelectTypesFilter`.

--- MediatorVH/Middleware/MediatorVhExt.cs
+++ MediatorVH/Middleware/MediatorVhExt.cs
@@ -1,5 +1,7 @@
 ﻿using MediatorVH.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Scrutor;
 
 namespace MediatorVH.Middleware;
 
@@ -8,14 +10,28 @@ public static class MediatorVhExt
     public static IServiceCollection AddMediatorVh(this IServiceCollection services, IEnumerable<Type> types)
     {
         services.Scan(scan => scan.FromAssembliesOf(types)
+            .SelectTypesFilter());
+        return services;
+    }
+
+    public static IServiceCollection AddMediatorVh(this IServiceCollection services, IEnumerable<Assembly> assemblies)
+    {
+        services.Scan(scan =>
+            scan.FromAssemblies(assemblies).SelectTypesFilter());
+        return services;
+    }
+
+    private static IImplementationTypeSelector SelectTypesFilter(this IImplementationTypeSelector typesSelector)
+    {
+        typesSelector
             .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<,>)), publicOnly: false)
             .AsImplementedInterfaces()
             .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<>)), publicOnly: false)
             .AsImplementedInterfaces()
             .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)), publicOnly: false)
             .AsImplementedInterfaces()
-            .WithScopedLifetime());
+            .WithScopedLifetime();
 
-        return services;
+        return typesSelector;
     }
 }
